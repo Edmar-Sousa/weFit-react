35ae1350aec596f750a385834787c1806ae608e5
edinho <edmarsousamendes4@gmail.com>
2024-03-21 10:33:29 -0300

    style: remove margin top

--- src/components/Loading/styled.ts
+++ src/components/Loading/styled.ts
@@ -1,17 +1,13 @@
 import styled from 'styled-components'
 
 export const LoaderImage = styled.img`
-    width: 62px;
-    height: 62px;
+    width: 100%;
+    max-width: 62px;
 
     display: block;
     margin: auto;
 
     animation: spin 1s linear infinite;
-
-    @media (min-width: 425px) {
-        margin-top: 40px;
-    }
 
     @keyframes spin {
         to {
